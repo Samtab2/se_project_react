4d2f95860769742fd2c3a7516c6471e76f117957
Sammy Tabally <sammytabally@hotmail.com>
2024-04-03 23:07:45 -0500

    New update for my project

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,21 +1,18 @@
 import "./ItemModal.css";
 
-function ItemModal({ onDelete, item, onClose }) {
+function ItemModal({ onDelete, card, onClose }) {
   return (
-    <div className={`modal`}>
+    <div className="modal__content">
       <div className="modal__content modal__content_type_image">
         <button onClick={onClose} type="button" className="modal__close">
         </button>
-        <img src={item?.imageUrl} alt={item?.name} className="modal__image" />
+        <img src={card.imageUrl} alt={card.name} className="modal__image" />
         <div className="modal__footer">
-           <h2 className="modal__caption">{item?.name}</h2>
-           <div className="modal__content-name">
-           {" "} {item?.name}
+           <h2 className="modal__caption">{card.name}</h2>
            <button onClick={onDelete} type="button" className="modal__delete"> Delete item </button>
-           <p className="modal__weather"> Weather: {item?.weather}</p>
+           <p className="modal__weather"> Weather: {card.weather}</p>
         </div>
         </div>
-      </div>
     </div>
   );
 }
