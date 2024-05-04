32281e2b0d9c60cf78fda42a2eb8a528745d60d7
Sammy Tabally <sammytabally@hotmail.com>
2024-05-03 22:26:54 -0500

    New update for my project

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -81,7 +81,7 @@ function App() {
   };
 
   const openConfirmationModal = () => {
-    setIsConfirmationModalOpen(true);
+    isConfirmationModalOpen(true);
     setIsConfirmationModalOpen(false);
   };
 
