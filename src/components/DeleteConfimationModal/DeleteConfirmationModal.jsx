b5ba2b5509343ce297cbc32d5b1006ac90fae8cb
Sammy Tabally <sammytabally@hotmail.com>
2024-05-05 21:33:34 -0500

    New update for my project

--- src/components/DeleteConfimationModal/DeleteConfirmationModal.jsx
+++ src/components/DeleteConfimationModal/DeleteConfirmationModal.jsx
@@ -1,44 +1,22 @@
-import "./DeleteConfirmationModal.css"
+import "./DeleteConfirmationModal.css";
 
-
-
-function DeleteConfirmationModal({ onDelete, onCancel, onConfirm }) {
-
-   const handleDelete = (e) => {
-     e.preventDefault();
-     onConfirm();
-   };
-
-
-
-    return (
-     <div className="modal-overlay">
-     <div className="modal">
-     <div className="modal__content">
-     <p className="modal__text">
-     Are you sure you want to delete {onDelete.name}?
-     </p>
-     <div className="modal__buttons">
-     <button
-     type="button"
-     className="modal__button"
-     onClick={onCancel}
-     >
-     Cancel
-     </button>
-     <button
-     type="button"
-     className="modal__button"
-     onClick={handleDelete}
-     >
-     Delete
-     </button>
-     </div>
-     </div>
-
-     </div>
-     </div>
-
-    );
-  }
-  export default DeleteConfirmationModal;
+function DeleteConfirmationModal({ onDelete, onCancel, onConfirm, isOpen }) {
+  return (
+    <div className={`modal ${isOpen ? "modal_opened" : ""}`}>
+      <div className="modal__content">
+        <p className="modal__text">
+          Are you sure you want to delete {onDelete.name}?
+        </p>
+        <div className="modal__buttons">
+          <button type="button" className="modal__button" onClick={onCancel}>
+            Cancel
+          </button>
+          <button type="button" className="modal__button" onClick={onConfirm}>
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default DeleteConfirmationModal;
